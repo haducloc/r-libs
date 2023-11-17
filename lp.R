80f4a179f59661218524204e31a48d22ac5d69e7
Loc Ha <haducloc13@gmail.com>
2023-11-17 16:32:20 -0600

    Update lp.R

--- lp.R
+++ lp.R
@@ -1,54 +1,59 @@
 lp_df_sum_ <- function(df) {
-  # Validate
-  if (!is.data.frame(df) && !inherits(df, "tbl_df")) {
-    stop("The given df must be a data frame or tibble.")
+  
+  get_distinct_values <- function(column_values) {
+    unique_values <- unique(column_values)
+    output <- character(0)
+  
+    total_values <- length(unique_values)
+  
+    for (i in seq_along(unique_values)) {
+      current_value <- unique_values[i]
+      current_value_str <- as.character(current_value)
+  
+      if (length(output) == 0) {
+        output <- current_value_str
+      } else {
+        current_output <- paste(output, current_value_str, sep = ", ")
+        if (nchar(current_output) <= 32) {
+          output <- current_output
+        } else {
+          output <- paste(output, ", ... more", sep = "")
+          break
+        }
+      }
+    }
+  
+    return (paste(output, "(", total_values, " values)"))
+  }
+
+  # List of Columns
+  
+  result <- paste("***** Data Frame:", paste(ncol(df), "columns x", nrow(df), "rows\n\n"))
+                  
+  for (i in 1:ncol(df)) {
+    col_name <- names(df)[i]
+    col_type <- class(df[[col_name]])
+    has_na <- any(is.na(df[[col_name]]))
+    is_unique <- length(unique(df[[col_name]])) == nrow(df)
+    
+    result <- paste(result, i, "-", col_name, ":", col_type,
+                    if (has_na) ", has NA(s)" else "",
+                    if (is_unique) ", is UNIQUE\n" else "\n")
+  }
+  result <- paste(result, "\n")
+  
+  # Column Distinct Values
+  result <- paste(result, "***** Column distinct values:\n\n")
+  for (i in 1:ncol(df)) {
+    col_name <- names(df)[i]
+    distinct_values <- get_distinct_values(df[[col_name]])
+    result <- paste(result, i, "-", col_name, ":", distinct_values, "\n")
   }
   
-  # Duplicate
-  has_duplicates <- any(duplicated(df))
-  
-  # unique
-  unique_cols <- names(df)[sapply(df, function(col) length(unique(col)) == nrow(df))]
-  non_unique_cols <- setdiff(names(df), unique_cols)
-  
-  # NA
-  na_cols <- names(df)[sapply(df, function(col) any(is.na(col)))]
-  non_na_cols <- setdiff(names(df), na_cols)
-  
-  # Column Name & Types
-  not_snake_case_cols <- names(df)[!grepl("^[a-z_][a-z0-9_]*$", names(df))]
-  
-  col_info <- sapply(names(df), function(col) paste(col, ":", typeof(df[[col]])))
-  df_desc <- paste(col_info, collapse = "|")
-  
-  summary_string <- paste(
-    "\n\n|----------------------Data Frame/Tibble Summary------------------------",
-    "\n", df_desc,
-    "\n",
-    "\n1. Duplicate rows? ", ifelse(has_duplicates, "Yes", "No"),
-    "\n2. Unique: ", paste(unique_cols, collapse = ", "),
-    "\n3. NOT unique: ", paste(non_unique_cols, collapse = ", "),
-    "\n4. NA: ", paste(na_cols, collapse = ", "),
-    "\n5. NOT NA: ", paste(non_na_cols, collapse = ", "),
-    "\n6. NOT snake_case: ", paste(not_snake_case_cols, collapse = ", "),
-    "\n------------------------------------------------------------------------|",
-    "\n\n"
-  )
-  return(summary_string)
+  return(paste(result, "\n"))
 }
 
 lp_df_sum <- function(df) {
   info <- lp_df_sum_(df)
   cat(info)
-}
-
-lp_df_values <- function(df, columns) {
-  result <- character(length(columns))
-  
-  for (i in seq_along(columns)) {
-    col <- columns[i]
-    unique_values <- unique(df[[col]])
-    result[i] <- paste("Unique values for column ", col, ": ", toString(unique_values))
-  }
-  return(result)
 }
